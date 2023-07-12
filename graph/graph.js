3b77fa7c340d7bae8f42b6ffc4597f9a79468da2
TuongNghiBui <camellia.bui95@gmail.com>
2023-07-12 21:09:45 +0200

    creating graph

--- graph/graph.js
+++ graph/graph.js
@@ -4,4 +4,24 @@ class Graph {
     }
 }
 
+class Node {
+    name;
+    edges = [];
+    constructor(name) {
+        this.name = name;
+    }
+    addEdge(toNode, weight) {
+        this.edges.push(new Edge(this, toNode, weight));
+    }
+
+}
+
+class Edge {
+    constructor(fromNode, toNode, weight) {
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+        this.weight = weight;
+    }
+}
+
 export default Graph;
